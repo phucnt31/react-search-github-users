89c27ddb0952fa78cddcce6e8f8670dc2aea5e23
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-11-10 21:24:49 +0700

    Setup and render doughnut chart

--- src/components/Charts/Doughnut2d.jsx
+++ src/components/Charts/Doughnut2d.jsx
@@ -1,7 +1,81 @@
+// STEP 1 - Include Dependencies
+// Include react
 import React from "react";
+import ReactDOM from "react-dom";
 
-const Doughnut2d = () => {
-  return <div>Doughnut2d</div>;
+// Include the react-fusioncharts component
+import ReactFC from "react-fusioncharts";
+
+// Include the fusioncharts library
+import FusionCharts from "fusioncharts";
+
+// Include the chart type
+import Column2D from "fusioncharts/fusioncharts.charts";
+
+// Include the theme as fusion
+import FusionTheme from "fusioncharts/themes/fusioncharts.theme.candy";
+
+// Adding the chart and theme as dependency to the core fusioncharts
+ReactFC.fcRoot(FusionCharts, Column2D, FusionTheme);
+
+// STEP 2 - Chart Data
+const chartData = [
+  {
+    label: "Venezuela",
+    value: "290",
+  },
+  {
+    label: "Saudi",
+    value: "260",
+  },
+  {
+    label: "Canada",
+    value: "180",
+  },
+  {
+    label: "Iran",
+    value: "140",
+  },
+  {
+    label: "Russia",
+    value: "115",
+  },
+  {
+    label: "UAE",
+    value: "100",
+  },
+  {
+    label: "US",
+    value: "30",
+  },
+  {
+    label: "China",
+    value: "30",
+  },
+];
+
+// STEP 3 - Creating the JSON object to store the chart configurations
+const ChartComponent = ({ data }) => {
+  const chartConfigs = {
+    type: "doughnut2d", // The chart type
+    width: "100%", // Width of the chart
+    height: "400", // Height of the chart
+    dataFormat: "json", // Data type
+    dataSource: {
+      // Chart Configuration
+      chart: {
+        caption: "Stars Per Language",
+        decimals: 0,
+        doughnutRadius: "45%",
+        showPercentValues: 0,
+        theme: "candy",
+      },
+      // Chart Data
+      data: chartData,
+    },
+  };
+
+  return <ReactFC {...chartConfigs} />;
 };
 
-export default Doughnut2d;
+export default ChartComponent;
