799b1962fa62fe6883e2c7665af6e4ae89ed4157
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-11-10 19:31:04 +0700

    Create Error.jsx and setup route for error page

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { Dashboard, Login } from "./pages";
+import { Dashboard, Error, Login } from "./pages";
 
 const App = () => {
   return (
     <Router>
-      <Route path="/" exact={true}>
-        <Dashboard />
-      </Route>
-      <Route path="/login">
-        <Login />
-      </Route>
+      <Switch>
+        <Route path="/" exact={true}>
+          <Dashboard />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="*">
+          <Error />
+        </Route>
+      </Switch>
     </Router>
   );
 };
