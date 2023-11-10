9cb04f5a73297622ec34fc81c88b0cced3186009
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-11-10 19:37:23 +0700

    Load newly created components in Dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import loadingImage from "../images/preloader.gif";
+import { Info, Repos, User, Search, Navbar } from "../components";
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <main>
+      <Navbar />
+      <Search />
+      <Info />
+      <User />
+      <Repos />
+    </main>
+  );
 };
 
 export default Dashboard;
