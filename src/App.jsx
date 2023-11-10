495d808ad1ffd43505f24bd7313d3a5410b8a50e
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-11-10 19:23:58 +0700

    Setup react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Dashboard, Login } from "./pages";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Router>
+      <Route path="/" exact={true}>
+        <Dashboard />
+      </Route>
+      <Route path="/login">
+        <Login />
+      </Route>
+    </Router>
+  );
 };
 
 export default App;
