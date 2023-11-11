b03b906dbe75beb0ad7b287dd8cd7ea57cc63564
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-11-11 12:27:34 +0700

    Refactor react router to v6

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,23 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Dashboard, Error, Login, PrivateRoute, AuthWrapper } from "./pages";
 
 const App = () => {
   return (
     <AuthWrapper>
       <Router>
-        <Switch>
-          <PrivateRoute path="/" exact={true}>
-            <Dashboard />
-          </PrivateRoute>
-          <Route path="/login">
-            <Login />
-          </Route>
-          <Route path="*">
-            <Error />
-          </Route>
-        </Switch>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <PrivateRoute>
+                <Dashboard />
+              </PrivateRoute>
+            }
+          />
+          <Route path="login" element={<Login />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
       </Router>
     </AuthWrapper>
   );
