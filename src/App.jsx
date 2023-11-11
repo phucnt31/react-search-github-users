f3641a9a2a0b410140a654bce3a462497a89d82f
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-11-11 12:20:32 +0700

    Create AuthWrapper.jsx and modified it to fix protected route bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,24 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { Dashboard, Error, Login, PrivateRoute } from "./pages";
+import { Dashboard, Error, Login, PrivateRoute, AuthWrapper } from "./pages";
 
 const App = () => {
   return (
-    <Router>
-      <Switch>
-        <PrivateRoute path="/" exact={true}>
-          <Dashboard />
-        </PrivateRoute>
-        <Route path="/login">
-          <Login />
-        </Route>
-        <Route path="*">
-          <Error />
-        </Route>
-      </Switch>
-    </Router>
+    <AuthWrapper>
+      <Router>
+        <Switch>
+          <PrivateRoute path="/" exact={true}>
+            <Dashboard />
+          </PrivateRoute>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="*">
+            <Error />
+          </Route>
+        </Switch>
+      </Router>
+    </AuthWrapper>
   );
 };
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
+import AuthWrapper from "./AuthWrapper";
 import Dashboard from "./Dashboard";
 import Error from "./Error";
 import Login from "./Login";
 import PrivateRoute from "./PrivateRoute";
 
-export { Dashboard, Login, Error, PrivateRoute };
+export { Dashboard, Login, Error, PrivateRoute, AuthWrapper };
