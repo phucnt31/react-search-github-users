65c39a63bcbc518f42afa0e56809cf957a806784
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-11-11 10:04:12 +0700

    Render bar and column chart

--- src/components/Repos.jsx
+++ src/components/Repos.jsx
@@ -35,11 +35,29 @@ const Repos = () => {
     })
     .slice(0, 5);
 
+  // stars, forks
+  let { stars, forks } = repos.reduce(
+    (total, item) => {
+      const { stargazers_count, name, forks } = item;
+      total.stars[stargazers_count] = { label: name, value: stargazers_count };
+      total.forks[forks] = { label: name, value: forks };
+      return total;
+    },
+    {
+      stars: {},
+      forks: {},
+    }
+  );
+  stars = Object.values(stars).slice(-5).reverse();
+  forks = Object.values(forks).slice(-5).reverse();
+
   return (
     <section className="section">
       <Wrapper className="section-center">
         <Pie3D data={mostUsed} />
+        <Column3D data={stars} />
         <Doughnut2D data={mostPopular} />
+        <Bar3D data={forks} />
       </Wrapper>
     </section>
   );
